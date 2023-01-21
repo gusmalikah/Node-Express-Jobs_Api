1a83d58971d5b2b7626ee4fb2b5e2184c0d9c763
unknown <hannanmalik1999@gmail.com>
2023-01-21 21:12:39 +0100

    Models nad routes basic setup. Auth controller started

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "please provide name"],
+        minLength: 3,
+        maxLEngth: 50
+    },
+    email: {
+        type: String,
+        required: [true, "please provide email"],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please provide valid email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "please provide name"],
+        minLength: 6,
+    }
+})
+
+module.exports = mongoose.model("User",UserSchema)
